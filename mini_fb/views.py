2abc0dc136e91854186411c3a6c25a9a24f848a6
kevingma <kevingma@bu.edu>
2024-10-07 21:33:35 -0400

    docstrings

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -4,6 +4,7 @@ from .models import Profile
 # Create your views here.
 
 class ShowAllProfilesView(ListView):
+    """Shows all profiles"""
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
